1a9d3e7da471df0daf784832b7ca9a1d1edb58a5
eadwinCode <ezeudoh.tochukwu@gmail.com>
2021-06-18 03:40:45 +0100

    removed local_install.sh

--- local_install.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-pip uninstall -y django-extra-ninja
-pip install django-extra-ninja --no-index --find-links=file:///home/eadwin/Desktop/django_ninja_extra_demo/django-ninja-extra/dist/django-ninja-extra-0.10.1.tar.gz
