139cb432d8941d7d663e22a94f5c14f8a220aaad
eadwinCode <ezeudoh.tochukwu@gmail.com>
2021-10-26 17:59:10 +0100

    Added test for lazy_import feature

---
+++ tests/test_lazy_import.py
@@ -0,0 +1,16 @@
+import pytest
+from ninja_extra.lazy import LazyStrImport
+
+
+class TestLazyClassImport:
+    """Test lazy import"""
+
+
+def test_lazy_str_import_works():
+    lazy_import = LazyStrImport('tests.test_lazy_import.TestLazyClassImport')
+    instance = lazy_import()
+    assert isinstance(instance, TestLazyClassImport)
+
+    with pytest.raises(Exception) as exc:
+        lazy_import = LazyStrImport('tests.test_lazy_import.InvalidLazyClassImport')
+        lazy_import()
