fb078aa31512048e06e0ecac586a2c0caa30cd45
eadwinCode <ezeudoh.tochukwu@gmail.com>
2021-10-26 08:24:40 +0100

    Added LazyStrImport class for lazy file import

--- ninja_extra/__init__.py
+++ ninja_extra/__init__.py
@@ -21,4 +21,5 @@ __all__ = [
     "shortcuts",
     "get_injector",
     "service_resolver",
+    "lazy",
 ]
---
+++ ninja_extra/lazy.py
@@ -0,0 +1,23 @@
+from typing import no_type_check
+
+from django.utils.functional import LazyObject, empty
+from django.utils.module_loading import import_string
+
+
+class LazyStrImport(LazyObject):
+    def __init__(self, import_str: str):
+        self.__dict__["_import_str"] = import_str
+        super(LazyStrImport, self).__init__()
+
+    @no_type_check
+    def _setup(self):
+        if self._wrapped is empty:
+            self._wrapped = import_string(self._import_str)
+            return self._wrapped
+        return self._wrapped
+
+    @no_type_check
+    def __call__(self, *args, **kwargs):
+        if self._wrapped is empty:
+            self._setup()
+        return self._wrapped(*args, **kwargs)
