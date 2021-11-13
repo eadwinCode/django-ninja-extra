12bb37c329d9f56d1dd748e830854e023ac80e24
eadwinCode <ezeudoh.tochukwu@gmail.com>
2021-11-13 23:56:08 +0100

    0.12.1

--- ninja_extra/__init__.py
+++ ninja_extra/__init__.py
@@ -1,6 +1,6 @@
 """Django Ninja Extra - Class Based Utility and more for Django Ninja(Fast Django REST framework)"""
 
-__version__ = "0.12.0"
+__version__ = "0.12.1"
 
 from ninja_extra.controllers import APIController
 from ninja_extra.controllers.route import route
