60dcb0fb6e45152c936c01a75aecbf3c496cf358
eadwinCode <ezeudoh.tochukwu@gmail.com>
2022-06-26 15:20:23 +0100

    code refactor

--- ninja_extra/compatible.py
+++ ninja_extra/compatible.py
@@ -1,6 +1,6 @@
 import sys
 
 if sys.version_info >= (3, 7):
-    from contextlib import asynccontextmanager as asynccontextmanager
+    from contextlib import asynccontextmanager as asynccontextmanager  # noqa
 else:
     from contextlib2 import asynccontextmanager as asynccontextmanager  # noqa
