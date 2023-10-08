163c9cd7bb08b0e6fd66ea5fe1a88dc5e2e00200
Ezeudoh Tochukwu <ezeudoh.tochukwu@gmail.com>
2023-10-08 18:52:10 +0100

    revert ninja extra settings to pydantic v1

--- ninja_extra/conf/settings.py
+++ ninja_extra/conf/settings.py
@@ -1,10 +1,8 @@
-import typing as t
+from typing import Any, Dict, List, Optional
 
 from django.conf import settings as django_settings
 from django.core.signals import setting_changed
-from ninja import Schema
-from pydantic import Field, field_validator
-from pydantic.config import ConfigDict
+from pydantic.v1 import BaseModel, Field, root_validator, validator
 
 from ninja_extra.lazy import LazyStrImport
 
@@ -14,7 +12,7 @@ class UserDefinedSettingsMapper:
         self.__dict__ = data
 
 
-NinjaExtra_SETTINGS_DEFAULTS = {
+NinjaEXTRA_SETTINGS_DEFAULTS = {
     "INJECTOR_MODULES": [],
     "PAGINATION_CLASS": "ninja_extra.pagination.LimitOffsetPagination",
     "THROTTLE_CLASSES": [
@@ -27,86 +25,80 @@ NinjaExtra_SETTINGS_DEFAULTS = {
 }
 
 USER_SETTINGS = UserDefinedSettingsMapper(
-    getattr(django_settings, "NINJA_EXTRA", NinjaExtra_SETTINGS_DEFAULTS)
+    getattr(django_settings, "NINJA_EXTRA", NinjaEXTRA_SETTINGS_DEFAULTS)
 )
 
 
-class NinjaExtraSettings(Schema):
-    model_config = ConfigDict(from_attributes=True, validate_assignment=True)
+class NinjaExtraSettings(BaseModel):
+    class Config:
+        orm_mode = True
+        validate_assignment = True
 
-    PAGINATION_CLASS: t.Any = Field(
+    PAGINATION_CLASS: Any = Field(
         "ninja_extra.pagination.LimitOffsetPagination",
     )
     PAGINATION_PER_PAGE: int = Field(100)
-    THROTTLE_RATES: t.Dict[str, t.Optional[str]] = Field(
+    THROTTLE_RATES: Dict[str, Optional[str]] = Field(
         {"user": "1000/day", "anon": "100/day"}
     )
-    THROTTLE_CLASSES: t.List[t.Any] = []
-    NUM_PROXIES: t.Optional[int] = None
-    INJECTOR_MODULES: t.List[t.Any] = []
-    ORDERING_CLASS: t.Union[str, t.Type] = "ninja_extra.ordering.Ordering"
-    SEARCHING_CLASS: t.Any = Field(
+    THROTTLE_CLASSES: List[Any] = []
+    NUM_PROXIES: Optional[int] = None
+    INJECTOR_MODULES: List[Any] = []
+    ORDERING_CLASS: Any = Field(
+        "ninja_extra.ordering.Ordering",
+    )
+    SEARCHING_CLASS: Any = Field(
         "ninja_extra.searching.Searching",
     )
 
-    @field_validator("INJECTOR_MODULES", mode="before")
-    def pre_injector_module_validate(cls, value: t.Any) -> t.Any:
+    @validator("INJECTOR_MODULES", pre=True)
+    def pre_injector_module_validate(cls, value: Any) -> Any:
         if not isinstance(value, list):
             raise ValueError("Invalid data type")
-        return cls._validate_ninja_extra_settings(value)
-
-    @field_validator("THROTTLE_CLASSES", mode="before")
-    def pre_throttling_class_validate(cls, value: t.Any) -> t.Any:
-        if not isinstance(value, list):
-            raise ValueError("Invalid data type")
-        return cls._validate_ninja_extra_settings(value)
-
-    @field_validator("PAGINATION_CLASS", mode="before")
-    def pre_pagination_class_validate(cls, value: t.Any) -> t.Any:
-        if isinstance(value, list):
-            raise ValueError("Invalid data type")
-        return cls._validate_ninja_extra_settings(value)
-
-    @field_validator("ORDERING_CLASS", mode="before")
-    def pre_ordering_class_validate(cls, value: t.Any) -> t.Any:
-        if isinstance(value, list):
-            raise ValueError("Invalid data type")
-        return cls._validate_ninja_extra_settings(value)
-
-    @field_validator("SEARCHING_CLASS", mode="before")
-    def pre_searching_class_validate(cls, value: t.Any) -> t.Any:
-        if isinstance(value, list):
-            raise ValueError("Invalid data type")
-        return cls._validate_ninja_extra_settings(value)
-
-    # @model_validator(mode='after')
-    # def validate_after(self):
-    #     for item in NinjaExtra_SETTINGS_DEFAULTS.keys():
-    #         value = getattr(self, item)
-    #         if (
-    #             isinstance(value, (tuple, list))
-    #             and value
-    #             and isinstance(value[0], str)
-    #         ):
-    #             value = [LazyStrImport(str(klass)) for klass in value]
-    #         if isinstance(value, str):
-    #             value = LazyStrImport(value)
-    #
-    #         setattr(self, item, value)
-    #     return self
-    @classmethod
-    def _validate_ninja_extra_settings(cls, value: t.Any) -> t.Any:
-        if isinstance(value, (tuple, list)) and value and isinstance(value, str):
-            return [LazyStrImport(str(klass)) for klass in value]
-        if isinstance(value, str):
-            return LazyStrImport(value)
         return value
+
+    @validator("THROTTLE_CLASSES", pre=True)
+    def pre_throttling_class_validate(cls, value: Any) -> Any:
+        if not isinstance(value, list):
+            raise ValueError("Invalid data type")
+        return value
+
+    @validator("PAGINATION_CLASS", pre=True)
+    def pre_pagination_class_validate(cls, value: Any) -> Any:
+        if isinstance(value, list):
+            raise ValueError("Invalid data type")
+        return value
+
+    @validator("ORDERING_CLASS", pre=True)
+    def pre_ordering_class_validate(cls, value: Any) -> Any:
+        if isinstance(value, list):
+            raise ValueError("Invalid data type")
+        return value
+
+    @validator("SEARCHING_CLASS", pre=True)
+    def pre_searching_class_validate(cls, value: Any) -> Any:
+        if isinstance(value, list):
+            raise ValueError("Invalid data type")
+        return value
+
+    @root_validator
+    def validate_ninja_extra_settings(cls, values: Any) -> Any:
+        for item in NinjaEXTRA_SETTINGS_DEFAULTS.keys():
+            if (
+                isinstance(values[item], (tuple, list))
+                and values[item]
+                and isinstance(values[item][0], str)
+            ):
+                values[item] = [LazyStrImport(str(klass)) for klass in values[item]]
+            if isinstance(values[item], str):
+                values[item] = LazyStrImport(values[item])
+        return values
 
 
 settings = NinjaExtraSettings.from_orm(USER_SETTINGS)
 
 
-def reload_settings(*args: t.Any, **kwargs: t.Any) -> None:  # pragma: no cover
+def reload_settings(*args: Any, **kwargs: Any) -> None:  # pragma: no cover
     global settings
 
     setting, value = kwargs["setting"], kwargs["value"]
